7ab0b2ae2b673e77302b547b5290c74b8246b52e
Nandakumar T <nandakumart07@gmail.com>
2023-08-10 21:31:22 +0530

    dashboard completed

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,12 +1,86 @@
 const UserModel = require('../models/userModel');
+const OrderModel = require('../models/orderModel');
+const ProductModel = require('../models/productModel');
+const dashboardHelper = require('../helper/dashboardHelpers');
 const bcrypt = require('bcrypt');
 
 const dashboardAdminGet = async (req, res) => {
     try {
-        res.render('admin/dashBoard', { admin: req.session.admin })
-    } catch (error) {
+        const today=new Date()
+        today.setHours(0,0,0,0)
+        const yesterday=new Date()
+        yesterday.setDate(today.getDate() - 1);
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        const currentMonth = now.getMonth();
+
+        const currentMonthStartDate = new Date(currentYear,currentMonth,1,0,0,0);
+        const previousMonthStartDate = new Date(currentYear, currentMonth - 1, 1, 0, 0, 0);
+        const previousMonthEndDate = new Date(currentYear, currentMonth, 0, 23, 59, 59);
+
+        const promises = [
+            dashboardHelper.currentMonthRevenue(currentMonthStartDate,now),
+            dashboardHelper.previousMonthRevenue(previousMonthStartDate,previousMonthEndDate),
+            dashboardHelper.paymentMethodAmount(),
+            dashboardHelper.todayIncome(today,now),
+            dashboardHelper.yesterdayIncome(today,yesterday),
+            dashboardHelper.totalRevenue(),
+            OrderModel.find({status:"confirm"}).count(),
+            OrderModel.find({status:"delivered"}).count(),
+            UserModel.find({is_blocked:false,is_verified:true}).count(),
+            ProductModel.find({status:true}).count(),
+            dashboardHelper.dailyChart(),
+            dashboardHelper.categorySales()
+        ]
+        const results = await Promise.all(promises);
+        console.log(results);
+
+        const revenueCurrentMonth = results[0]
+        const revenuePreviousMonth = results[1]
+        const paymentMethodAmount = results[2]
+        const todayIncome = results[3]
+        const yesterdayIncome = results[4]
+        const totalRevenue = results[5]
+        const ordersToShip = results[6]
+        const completedOrders = results[7]
+        const userCount = results[8]
+        const productCount = results[9]
+        const dailyChart = results[10]
+        const categorySales = results[11]
+       
+        const razorPayAmount = paymentMethodAmount && paymentMethodAmount.length > 0 ? paymentMethodAmount.find(payment => payment._id === 'wallet' || payment._id === 'online')?.amount.toString() : 0;
+        // const razorPayAmount = paymentMethodAmount && paymentMethodAmount.length > 0 ? paymentMethodAmount.find(payment => payment._id === 'online')?.amount.toString() : 0;
+        const codPayAmount = paymentMethodAmount && paymentMethodAmount.length > 0 ? paymentMethodAmount.find(payment => payment._id === 'COD')?.amount.toString() : 0;
+        
+    
+        const monthlyGrowth = revenuePreviousMonth === 0 ? 100 : (((revenueCurrentMonth - revenuePreviousMonth)/revenuePreviousMonth)*100).toFixed(1);
+    
+        const dailyGrowth = (((todayIncome - yesterdayIncome)/yesterdayIncome)*100).toFixed(1);
+        console.log(monthlyGrowth);
+        console.log(dailyGrowth);
+        
+        res.render('admin/dashBoard', { 
+            admin: req.session.admin ,
+            todayIncome,
+            dailyGrowth,
+            totalRevenue,
+            revenueCurrentMonth,
+            monthlyGrowth,
+            // walletPayAmount,
+            razorPayAmount,
+            codPayAmount,
+            userCount,
+            ordersToShip,
+            completedOrders,
+            productCount,
+            dailyChart,
+            categorySales
+        })
+        
+      } catch (error) {
         console.log(error);
-    }
+
+      }
 }
 
 const bannerAdminGet = async (req, res) => {
---
+++ helper/dashboardHelpers.js
@@ -0,0 +1,107 @@
+const OrderModel = require('../models/orderModel');
+
+const currentMonthRevenue =async(currentMonthStratDate,now)=>{
+    console.log(currentMonthStratDate+"-----------currentMonthStratDate");
+    console.log(now+"-----------now");
+    const currentMonthRevenue = await OrderModel.aggregate([
+        {$match:{orderDate:{$gte:currentMonthStratDate,$lt:now},status:{$nin:[ "Pending","canceled","returned"]}}},
+        {$group:{_id:null,currentMonthRevenue:{$sum:"$totalPrice"}}}
+    ])
+    console.log(currentMonthRevenue);
+    const result = currentMonthRevenue.length > 0 ? currentMonthRevenue[0].currentMonthRevenue : 0;
+    return result
+
+}
+const previousMonthRevenue = async(previousMonthStratDate,previousMonthEndDate)=>{
+    const previousMonthRevenue = await OrderModel.aggregate([
+      {$match:{orderDate:{$gte:previousMonthStratDate,$lt:previousMonthEndDate},status:{$ne:"Pending"}}},
+      {$group:{_id:null,previousMonthRevenue:{$sum:"$totalPrice"}}}
+    ]);
+    const result = previousMonthRevenue.length > 0 ? previousMonthRevenue[0].previousMonthRevenue : 0;
+    return result;
+  }
+
+  const paymentMethodAmount  =async()=>{    const paymentMethodTotal =await OrderModel.aggregate([
+        {$match:{status:{$nin:[ "Pending","canceled","returned"]}}},
+        {$group:{_id:"$paymentMethod",amount:{$sum:"$totalPrice"}}}
+    ])
+    const result = paymentMethodTotal.length > 0 ? paymentMethodTotal : 0;
+    return result;
+  }
+  const todayIncome = async(today, now) =>{
+    const todayOrders = await OrderModel.aggregate([
+      {$match:{orderDate:{$gte:today,$lt:now}}},
+      {$unwind:"$products"},
+      {$group:{_id:null,todayIncome:{$sum:{$multiply:[{$toDouble:"$products.quantity"},{$toDouble:"$products.price"}]}}}},
+      {$project:{todayIncome:1}}
+    ]);
+    const todayIncome = todayOrders.length > 0 ? todayOrders[0].todayIncome : 0;
+    return todayIncome;
+  }
+  const yesterdayIncome = async(today,yesterday) =>{
+    const yesterdayOrders = await OrderModel.aggregate([
+      {$match:{orderDate:{$gte:yesterday,$lt:today},status:{$nin:[ "Pending","canceled","returned"]}}},
+      {$unwind:"$products"},
+      {$group:{_id:null,yesterdayIncome:{$sum:{$multiply:[{$toDouble:"$products.quantity"},{$toDouble:"$products.price"}]}}}},
+      {$project:{yesterdayIncome:1}}
+    ]);
+    const yesterdayIncome = yesterdayOrders.length > 0 ? yesterdayOrders[0].yesterdayIncome : 0;
+    return yesterdayIncome
+  }
+  const totalRevenue = async() =>{
+    const revenue = await OrderModel.aggregate([
+      {$match:{status:{$nin:[ "Pending","canceled","returned"]}}},
+      {$group:{_id:null,revenue:{$sum:"$totalPrice"}}}
+    ]);
+    const totalRevenue = revenue.length > 0 ? revenue[0].revenue : 0;
+    return totalRevenue;
+  }
+
+  const dailyChart = async () =>{
+    const dailyOrders = await OrderModel.aggregate([
+      {$match:{status:{$nin:[ "Pending","canceled","returned"]}}},
+      {$group:{_id:{$dateToString:{format:"%Y-%m-%d",date:"$orderDate"}},dailyrevenue:{$sum:"$totalPrice"}}},
+      {$sort:{_id:1}},
+      {$limit:14}
+    ]);
+  
+    const result = dailyOrders || 0;
+    return result;
+  }
+
+  const categorySales = async() => {
+    const catSales = await OrderModel.aggregate([
+      {$match:{status:{$nin:[ "Pending","canceled","returned"]}}},
+      {$unwind:"$products"},
+      {$lookup:{
+        from:"products",
+        localField:"products.product_id",
+        foreignField:"_id",
+        as: "categories"
+      }},
+      {$unwind:"$categories"},
+      {$lookup:{
+        from:"categories",
+        localField:"categories.category",
+        foreignField:"_id",
+        as:"category"
+      }},
+      {$unwind:"$category"},
+      {$group:{_id:"$category.categoryName",qty:{$sum:"$products.quantity"}}}
+    ]);
+    // console.log(catSales);  
+    return catSales;
+  }
+  
+  
+
+module.exports={
+    currentMonthRevenue,
+    previousMonthRevenue,
+    paymentMethodAmount,
+    todayIncome,
+    yesterdayIncome,
+    totalRevenue,
+    dailyChart,
+    categorySales
+}
